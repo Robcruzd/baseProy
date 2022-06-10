5dc7002355ad489bcb341f45ec0e8aa2b4679fe6
Robinson Cruz Delgado <robcruzd@hotmail.com>
2022-06-10 01:18:18 -0500

    Initial version of base generated by generator-jhipster@7.8.1

---
+++ src/main/java/com/jave/base/config/LoggingAspectConfiguration.java
@@ -0,0 +1,17 @@
+package com.jave.base.config;
+
+import com.jave.base.aop.logging.LoggingAspect;
+import org.springframework.context.annotation.*;
+import org.springframework.core.env.Environment;
+import tech.jhipster.config.JHipsterConstants;
+
+@Configuration
+@EnableAspectJAutoProxy
+public class LoggingAspectConfiguration {
+
+    @Bean
+    @Profile(JHipsterConstants.SPRING_PROFILE_DEVELOPMENT)
+    public LoggingAspect loggingAspect(Environment env) {
+        return new LoggingAspect(env);
+    }
+}
---
+++ src/main/java/com/jave/base/service/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data Transfer Objects.
+ */
+package com.jave.base.service.dto;
